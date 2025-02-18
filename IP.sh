3f560ac48c5bf48876679836f798d0d73c597ed6
LR.flood_ofc <lucianogarcialuciano15@gmail.com>
2025-02-18 00:11:01 -0300

    IP.sh
    Script de enviar pacote de dados para sites.

---
+++ IP.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+#cor
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+echo " {RED}_____________________
+|olá, seja bem vind@.
+|painel criado pela formação;
+|  🎩💻🎩
+|criada com intuito algum.
+|pode usar este script para enviar
+|pacotes de dds a sites fazendo eles
+|cairem
+ --------- dominio sem https>>"
+read dominio
+echo "{GREEN}um ex: caso o dominio for, https://fln.app/flood_ajuda vc deve tirar o https e fazer assim. fln.app/flood_ajuda.com"
+sleep 3
+echo "{RED}creditos do yt: @fx bruxo wa"
+sleep 2
+ping $https>>
+#aumentado kb atk￼Enter
